8efc5fe40d95427c4977a787f23a5ad9bd35061b
Ludwig T. Tröller <ltroller@digital-village.fr>
2018-07-20 17:52:45 +0200

    final commit

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,141 +1,209 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import api from '../api';
-import ReactAudioPlayer from 'react-audio-player';
-import {Button, ButtonGroup} from 'reactstrap';
-import pic3 from '../pic3.jpg';
-import bg2 from '../bg2.jpg';
-import { Progress } from 'reactstrap';
-import { Route, Link, Switch } from 'react-router-dom';
+import api from "../api";
+import ReactAudioPlayer from "react-audio-player";
+import { Button, ButtonGroup } from "reactstrap";
+import pic3 from "../pic3.jpg";
+import bg2 from "../bg2.jpg";
+import { Progress } from "reactstrap";
+import { Route, Link, Switch } from "react-router-dom";
 
 class Game extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      game: null,
-      index: null,
-      isShowQuestion: true,
-      questionIndex: 0,
-      score: 0,
-      beginnigTime: Date.now()
-    };
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-  }
-  
-
-  onRadioBtnClick(iBtn) {
-    
-
-    let currentTime = Date.now()
-    let nbOfSeconds = Math.round((currentTime - this.state.beginnigTime) / 1000 ) 
-
-    let newScore = this.state.score
-    if (this.state.game.questions[this.state.questionIndex].answers[iBtn].isCorrect)
-      newScore += 100 - Math.min(nbOfSeconds, 30)
-    this.setState({
-      isShowQuestion: false,
-      score: newScore,
-      });
-    
-    this.setState({iBtn});
-  }
-
-  loadNextQuestion() {
-    setTimeout(()=>{
-      if (this.state.questionIndex+1 === 10) {
-        api.saveGameScore(this.props.match.params.gameId, this.state.score)
-      }
-      this.setState({
-        questionIndex: this.state.questionIndex+1,
-        iBtn: 4,
-        isShowQuestion: true,
-        beginnigTime: Date.now()
-      });
-    },1000)
-  }
-
-  componentDidMount() {
-    api.getGame(this.props.match.params.gameId)
-    .then(game => {
-      console.log("this is COMPONENTDIDMOUNT")
-      console.log("Should be a track", game)
-      this.setState({
-        game,
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-  getColor(i) {
-    if (this.state.isShowQuestion)
-      return "primary"
-    if (this.state.game.questions[this.state.questionIndex].answers[i].isCorrect)
-      return "success"
-      if (!this.state.game.questions[this.state.questionIndex].answers[i].isCorrect && this.state.iBtn===i)
-    return "danger"
-  }
-
-
-  render() {
-    if (!this.state.game)
-      return "Loading..."
-
-    let bestScore = this.state.game.players.reduce((max, val) => Math.max(max, val.score), 0)
-    console.log("GAME", this.state.game);
-    console.log("bestScore", bestScore);
-    
-    
-    if (this.state.questionIndex >= this.state.game.questions.length) {
-
-      return (
-        <div className = "container">
-          
-          <h3>Your Score is: {this.state.score}</h3>
-          <h3>Previous best score : {bestScore}</h3>
-          <h3>
-          <font face="georgia" size="5"color="secondary"> Share this same game with your friends: </font> </h3>
-          <font face="georgia" size="2"color="secondary"> {window.location.href} </font><br/>
-
-          
-          <img src={bg2} className="d-block w-100" alt="pic" />
-        </div>
-      )
+    constructor(props) {
+        super(props);
+        this.state = {
+            game: null,
+            index: null,
+            isShowQuestion: true,
+            questionIndex: 0,
+            score: 0,
+            beginnigTime: Date.now()
+        };
+        this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
     }
-    
-    let answer=this.state.game.questions && this.state.game.questions[this.state.questionIndex].answers[0].answer
-    let answer2=this.state.game.questions && this.state.game.questions[this.state.questionIndex].answers[1].answer
-    let answer3=this.state.game.questions && this.state.game.questions[this.state.questionIndex].answers[2].answer
-    let answer4=this.state.game.questions && this.state.game.questions[this.state.questionIndex].answers[3].answer
-    let url=this.state.game.questions && this.state.game.questions[this.state.questionIndex].previewUrl
 
-    console.log("TEST", this.state.questions && this.state.questions[this.state.questionIndex]);
+    onRadioBtnClick(iBtn) {
+        let currentTime = Date.now();
+        let nbOfSeconds = Math.round(
+            (currentTime - this.state.beginnigTime) / 1000
+        );
 
-    
+        let newScore = this.state.score;
+        if (
+            this.state.game.questions[this.state.questionIndex].answers[iBtn]
+                .isCorrect
+        )
+            newScore += 100 - Math.min(nbOfSeconds, 30);
+        this.setState({
+            isShowQuestion: false,
+            score: newScore
+        });
 
-    return (
-      <div>
-      <div className="container mt-4" >
-             <Progress value={this.state.questionIndex} max="10" />
-      </div>
-        <h3><font face="courier"size="5"color="success">Score : {this.state.score}</font></h3>
+        this.setState({ iBtn });
+    }
 
-        <ReactAudioPlayer autoPlay={true} src={url} controls/> <br/><br/>
-        <Button color={this.getColor(0)} onClick={() => this.onRadioBtnClick(0)}  >
-          {answer}</Button><br/><br/>
-        <Button color={this.getColor(1)} onClick={() => this.onRadioBtnClick(1)} >
-          {answer2}</Button><br/><br/>
-        <Button color={this.getColor(2)} onClick={() => this.onRadioBtnClick(2)} >
-          {answer3}</Button><br/><br/>
-        <Button color={this.getColor(3)} onClick={() => this.onRadioBtnClick(3)}  >
-          {answer4}</Button>
-          <br/><br/>
-         <img src={pic3} className="pic" alt="pic" />
-        {this.state.iBtn<4 && this.loadNextQuestion.call(this)}
-                
+    loadNextQuestion() {
+        setTimeout(() => {
+            if (this.state.questionIndex + 1 === 10) {
+                api.saveGameScore(
+                    this.props.match.params.gameId,
+                    this.state.score
+                );
+            }
+            this.setState({
+                questionIndex: this.state.questionIndex + 1,
+                iBtn: 4,
+                isShowQuestion: true,
+                beginnigTime: Date.now()
+            });
+        }, 1000);
+    }
 
-        </div>
-    );
-  }
+    componentDidMount() {
+        api.getGame(this.props.match.params.gameId)
+            .then(game => {
+                console.log("this is COMPONENTDIDMOUNT");
+                console.log("Should be a track", game);
+                this.setState({
+                    game
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
+    getColor(i) {
+        if (this.state.isShowQuestion) return "primary";
+        if (
+            this.state.game.questions[this.state.questionIndex].answers[i]
+                .isCorrect
+        )
+            return "success";
+        if (
+            !this.state.game.questions[this.state.questionIndex].answers[i]
+                .isCorrect &&
+            this.state.iBtn === i
+        )
+            return "danger";
+    }
+
+    splitName(name) {
+        let array = name.split("---");
+        return (
+            <div>
+                <span className="artist">{array[0].trim()}</span>
+                <span className="song">{array[1].trim()}</span>
+            </div>
+        );
+    }
+
+    render() {
+        if (!this.state.game) return "Loading...";
+
+        let bestScore = this.state.game.players.reduce(
+            (max, val) => Math.max(max, val.score),
+            0
+        );
+        console.log("GAME", this.state.game);
+        console.log("bestScore", bestScore);
+
+        if (this.state.questionIndex >= this.state.game.questions.length) {
+            return (
+                <div className="container results-wrapper" id="game-container">
+                    <h3>
+                        Your Score is: {this.state.score}{" "}
+                        <i class="fas fa-check-circle" />
+                    </h3>
+                    <h3>Previous best score : {bestScore}</h3>
+                    <h3>
+                        <div className="share-with-friends">
+                            <i className="fas fa-share" />
+                            Share this same game with your friends:
+                            <div className="link-to-game">
+                                {window.location.href}
+                            </div>
+                        </div>
+                    </h3>
+
+                    <img src={bg2} className="d-block w-100" alt="pic" />
+                </div>
+            );
+        }
+
+        let answer =
+            this.state.game.questions &&
+            this.state.game.questions[this.state.questionIndex].answers[0]
+                .answer;
+        let answer2 =
+            this.state.game.questions &&
+            this.state.game.questions[this.state.questionIndex].answers[1]
+                .answer;
+        let answer3 =
+            this.state.game.questions &&
+            this.state.game.questions[this.state.questionIndex].answers[2]
+                .answer;
+        let answer4 =
+            this.state.game.questions &&
+            this.state.game.questions[this.state.questionIndex].answers[3]
+                .answer;
+        let url =
+            this.state.game.questions &&
+            this.state.game.questions[this.state.questionIndex].previewUrl;
+
+        console.log(
+            "TEST",
+            this.state.questions &&
+                this.state.questions[this.state.questionIndex]
+        );
+
+        return (
+            <div>
+                <div className="container mt-4">
+                    <Progress value={this.state.questionIndex} max="10" />
+                </div>
+                <h3>
+                    <div className="score-handler">
+                        Score : {this.state.score}
+                    </div>
+                </h3>
+                <ReactAudioPlayer autoPlay={true} src={url} controls /> <br />
+                <br />
+                <div className="answers-button-wrapper">
+                    <Button
+                        color={this.getColor(0)}
+                        onClick={() => this.onRadioBtnClick(0)}
+                    >
+                        <i>A</i> {this.splitName(answer)}
+                    </Button>
+
+                    <Button
+                        color={this.getColor(1)}
+                        onClick={() => this.onRadioBtnClick(1)}
+                    >
+                        <i>B</i>
+                        {this.splitName(answer2)}
+                    </Button>
+
+                    <Button
+                        color={this.getColor(2)}
+                        onClick={() => this.onRadioBtnClick(2)}
+                    >
+                        <i>C</i> {this.splitName(answer3)}
+                    </Button>
+
+                    <Button
+                        color={this.getColor(3)}
+                        onClick={() => this.onRadioBtnClick(3)}
+                    >
+                        <i>D</i> {this.splitName(answer4)}
+                    </Button>
+                </div>
+                <br />
+                <br />
+                <img src={pic3} className="pic" alt="pic" />
+                {this.state.iBtn < 4 && this.loadNextQuestion.call(this)}
+            </div>
+        );
+    }
 }
 
 export default Game;
--- server/routes/games.js
+++ server/routes/games.js
@@ -1,34 +1,35 @@
-var express = require('express');
-const Game = require('../models/Game')
-const Song = require('../models/Song')
-const SpotifyWebApi = require('spotify-web-api-node');
-const passport = require('passport');
-const jwt = require('jwt-simple');
-const config = require('../configs/index');
+var express = require("express");
+const Game = require("../models/Game");
+const Song = require("../models/Song");
+const SpotifyWebApi = require("spotify-web-api-node");
+const passport = require("passport");
+const jwt = require("jwt-simple");
+const config = require("../configs/index");
 
 var router = express.Router();
 // var artistList = require('../bin/seeds');
 // var helper=require('../helpers/helper');
 
-
-const clientId = '774af390dd2142dcadfcccc52d3e22c3',
-    clientSecret = 'a55a2507b3dc49479367d163f1b48e90';
+const clientId = "774af390dd2142dcadfcccc52d3e22c3",
+    clientSecret = "a55a2507b3dc49479367d163f1b48e90";
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+    clientId: clientId,
+    clientSecret: clientSecret
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err);
-});
-
-
-
+spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+        spotifyApi.setAccessToken(data.body["access_token"]);
+    },
+    function(err) {
+        console.log(
+            "Something went wrong when retrieving an access token",
+            err
+        );
+    }
+);
 
 // router.get('/random-old', (req, res, next) => {
 //   let promises = []
@@ -37,11 +38,10 @@ spotifyApi.clientCredentialsGrant()
 //   }
 //   Promise.all(promises)
 //   .then(questions => {
-//     let game = { 
+//     let game = {
 //       questions: questions
 //     }
-    
-    
+
 //     for (let i = 0; i < questions.length; i++) {
 //       if(game.questions[i])
 //         helper.shuffle(game.questions[i].answers)
@@ -57,73 +57,93 @@ spotifyApi.clientCredentialsGrant()
 //   })
 // });
 
+router.get("/random", (req, res, next) => {
+    Song.find()
+        .then(songs => {
+            console.log("Display songs =>", songs);
+            songs.sort((a, b) => Math.random() - 0.5); // To shuffle
+            console.log(
+                songs.map(song => song.artistName + " --- " + song.name)
+            );
 
-router.get('/random', (req, res, next) => {
-  Song.find()
-  .then(songs => {
-    console.log("Display songs =>", songs)
-    songs.sort((a,b) => Math.random()-0.5) // To shuffle
-    console.log(songs.map(song => song.artistName + " --- " + song.name));
+            let questions = [];
+            let nbOfQuestions = 10;
+            for (let i = 0; i < nbOfQuestions; i++) {
+                let answers = [
+                    {
+                        answer:
+                            '<span class="artist">' +
+                            songs[4 * i].artistName +
+                            "</span>" +
+                            songs[4 * i].name,
+                        isCorrect: true
+                    },
+                    {
+                        answer:
+                            '<span class="artist">' +
+                            songs[4 * i + 1].artistName +
+                            "</span>" +
+                            songs[4 * i + 1].name,
+                        isCorrect: false
+                    },
+                    {
+                        answer:
+                            '<span class="artist">' +
+                            songs[4 * i + 2].artistName +
+                            "</span>" +
+                            songs[4 * i + 2].name,
+                        isCorrect: false
+                    },
+                    {
+                        answer:
+                            '<span class="artist">' +
+                            songs[4 * i + 3].artistName +
+                            "</span>" +
+                            songs[4 * i + 3].name,
+                        isCorrect: false
+                    }
+                ];
 
-    let questions = []
-    let nbOfQuestions = 10
-    for (let i = 0; i < nbOfQuestions; i++) {
-      let answers = [
-        {
-          answer: songs[4*i].artistName + " --- " + songs[4*i].name,
-          "isCorrect": true,
-        },
-        {
-          answer: songs[4*i+1].artistName + " --- " + songs[4*i+1].name,
-          "isCorrect": false,
-        },
-        {
-          answer: songs[4*i+2].artistName + " --- " + songs[4*i+2].name,
-          "isCorrect": false,
-        },
-        {
-          answer: songs[4*i+3].artistName + " --- " + songs[4*i+3].name,
-          "isCorrect": false,
-        }
-      ]
+                answers.sort((a, b) => Math.random() - 0.5); // To shuffle
 
-      answers.sort((a,b) => Math.random()-0.5) // To shuffle
-
-      questions.push({
-        previewUrl: songs[4*i].previewUrl,
-        answers
-      })
-    }
-    let game = { 
-      questions: questions
-    }
-    return Game.create(game)
-  })
-  .then(gameCreated => {
-    res.json(gameCreated)
-  })
-  .catch((err) => {
-    console.log('Something went wrong!', err);
-    next(err)
-  })
+                questions.push({
+                    previewUrl: songs[4 * i].previewUrl,
+                    answers
+                });
+            }
+            let game = {
+                questions: questions
+            };
+            return Game.create(game);
+        })
+        .then(gameCreated => {
+            res.json(gameCreated);
+        })
+        .catch(err => {
+            console.log("Something went wrong!", err);
+            next(err);
+        });
 });
 
-router.get('/:gameId', (req, res, next) => {
-  Game.findById(req.params.gameId)
-  .populate("players._users")
-  .then(games => res.json(games))
-})
+router.get("/:gameId", (req, res, next) => {
+    Game.findById(req.params.gameId)
+        .populate("players._users")
+        .then(games => res.json(games));
+});
 
-
-router.post('/:gameId/score', passport.authenticate("jwt", config.jwtSession), (req, res, next) => {
-  let {score} = req.body
-  let elementPushed = {
-    _user: req.user._id,
-    score
-  }
-  Game.findByIdAndUpdate(req.params.gameId, {$push: { players: elementPushed}})
-  .then(game => res.json(game))
-})
-
+router.post(
+    "/:gameId/score",
+    passport.authenticate("jwt", config.jwtSession),
+    (req, res, next) => {
+        let { score } = req.body;
+        let elementPushed = {
+            _user: req.user._id,
+            score
+        };
+        Game.findByIdAndUpdate(req.params.gameId, {
+            $push: { players: elementPushed }
+        }).then(game => res.json(game));
+    }
+);
 
 module.exports = router;
